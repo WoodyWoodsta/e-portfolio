f81689905b4e004a81fbe1fd57ea947477fdc9a2
WoodyWoodsta <woodywoodsta@gmail.com>
2016-02-29 00:27:22 +0200

    add pages and footer

---
+++ elements/site-footer/site-footer.js
@@ -0,0 +1,3 @@
+Polymer({
+  is: 'site-footer'
+});
--- elements/site-header-bar/site-header-bar.js
+++ elements/site-header-bar/site-header-bar.js
@@ -7,10 +7,34 @@ Polymer({
       value: 0,
       observer: '_onPositionTopChanged'
     },
+
+    titleText: {
+      type: String
+    }
+  },
+
+  listeners: {
+    'iron-activate': 'onIronActivate'
   },
 
   attached() {
     this.fire('iron-activate', {selected: 'homepage'});
+  },
+
+  onIronActivate(e) {
+    console.log('Getting here');
+    switch (e.detail.selected) {
+      case 'homepage':
+        this.titleText = `me.e-portfolio.homepage`;
+        break;
+      case 'cv':
+        this.titleText = `me.e-portfolio.cv`;
+        break;
+      case 'social':
+        this.titleText = `me.e-portfolio.social`;
+        break;
+      default:
+    }
   },
 
   _onPositionTopChanged(newValue) {
