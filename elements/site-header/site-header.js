de4158b928839cb35572ed3a447b0a9c713cce43
WoodyWoodsta <woodywoodsta@gmail.com>
2016-02-28 11:01:19 +0200

    fix header sizing, add social buttons

--- elements/site-header/site-header.js
+++ elements/site-header/site-header.js
@@ -6,27 +6,41 @@ Polymer({
   properties: {
     _headerBarHeight: {
       type: Number
+    },
+
+    _headerHeight: {
+      type: Number
     }
   },
 
   attached() {
     window.addEventListener('scroll', this._onScroll.bind(this));
+    this._updateHeaderBarStyle();
+    this._updateHeaderContentStyle();
   },
 
   detached() {
     window.removeEventListener('scroll', this._onScroll.bind(this));
   },
 
+  _updateHeights() {
+    this._headerBarHeight = this.$.siteHeaderBar.getBoundingClientRect().height;
+    this._headerHeight = this.getBoundingClientRect().height;
+  },
+
   _updateHeaderContentStyle() {
+    this._updateHeights();
+
     this.transform(`translateY(${window.scrollY/3}px)`, this.$.headerContent);
     this.$.headerContent.style.opacity = `${(window.innerHeight - window.scrollY*1.2)/window.innerHeight}`;
   },
 
   _updateHeaderBarStyle() {
     var headerBarBottom = this.getBoundingClientRect().bottom;
-    this._headerBarHeight = this.$.siteHeaderBar.getBoundingClientRect().height;
     var pos = headerBarBottom - this._headerBarHeight;
-    this.$.siteHeaderBar.style.opacity = `${1 - 3*((window.innerHeight - this._headerBarHeight) - window.scrollY)/(window.innerHeight - this._headerBarHeight)}`;
+    this._updateHeights();
+
+    this.$.siteHeaderBar.style.opacity = `${1 - 3*((this._headerHeight - this._headerBarHeight) - window.scrollY)/(this._headerHeight - this._headerBarHeight)}`;
 
     this.$.siteHeaderBar.positionTop = (pos <= 0) ? 0 : pos;
   },
